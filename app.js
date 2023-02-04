e7eaa266c738d1a0b8e3fe1e2d55ff8edcfee47c
saurabhjha137 <saurabhjha137@gmail.com>
2023-02-04 12:58:59 +0530

    final commit

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const http = require("http");
+const http = require('http');
+const url = require('url');
+const moment = require('moment');
 
-const host = 'localhost';
-const port = 8000;
+// function to calculate change in date if any
+function calulateDateChange(query) {
+    const operation = query.operation;
+    let value = parseInt(query.value);
+    const dayOrWeek = query.dayOrWeek;
+    let date = query.date;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
+    //converting today to today's date
+    if (date === 'today'){
+        date = moment().format('DD-MMM-YYYY');
+    }
+    //converting week into days for easy calculation
+    if (dayOrWeek === 'week'){
+        value = parseInt(7*value);
+    }
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    //based on add/subtract performing action
+    if (operation === 'add'){
+        return moment(date).add(value, 'days').format('DD-MMM-YYYY');
+    } else if(operation === 'subtract'){
+        return moment(date).subtract(value, 'days').format('DD-MMM-YYYY');
+    }
+
+}
+
+
+const server = http.createServer((req, res) => {
+    
+    const query = url.parse(req.url, true).query;
+    let result = '';
+    result = calulateDateChange(query);
+        
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(result);
+
 });
 
+server.listen(8080, () => {
+    console.log('Server listening on http://localhost:8080');
+});
